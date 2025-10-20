527c660843bb03daee093ecf2ddbbeec3142d8b8
Stavoker <sros010295@gmail.com>
2025-10-20 19:28:36 +0100

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const moment = require('moment');
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
